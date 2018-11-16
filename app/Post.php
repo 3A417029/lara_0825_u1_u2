9b5d24afbe3d2d43536961ec69fee5ea46247328
3A417029 <howardzhu0709@outlook.com>
2018-11-16 17:49:57 +0800

    練習 5：資料表關聯

--- app/Post.php
+++ app/Post.php
@@ -3,15 +3,22 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Comment;
 
 class Post extends Model
 {
-    //
-    protected$table='posts';
-    
+    //    
+    /*
+    protected $table = 'posts';
     protected $fillable = [
         'title',
         'content',
         'is_feature'
     ];
+    */
+    public function comments()
+    {
+        return $this -> hasMany(Comment::class);
+    }
+
 }
