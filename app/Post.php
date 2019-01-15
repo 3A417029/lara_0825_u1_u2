b7379f558e7a24a3427779401a9e6463c7f7c656
3A417029 <howardzhu0709@outlook.com>
2019-01-15 12:48:23 +0800

    修正：comments 資料表 & Post.php

--- app/Post.php
+++ app/Post.php
@@ -8,14 +8,14 @@ use App\Comment;
 class Post extends Model
 {
     //    
-    /*
+    
     protected $table = 'posts';
     protected $fillable = [
         'title',
         'content',
         'is_feature'
     ];
-    */
+    
     public function comments()
     {
         return $this -> hasMany(Comment::class);
